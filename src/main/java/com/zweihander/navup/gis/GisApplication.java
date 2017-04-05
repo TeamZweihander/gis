972edf91d3f340ee3492dde6dbdea71e36bc7037
gershom12 <u13229908@tuks.co.za>
2017-04-05 12:36:48 +0200

    GIS springboot project

--- src/main/java/com/zweihander/navup/gis/GisApplication.java
+++ src/main/java/com/zweihander/navup/gis/GisApplication.java
@@ -1,9 +1,14 @@
-package com.zweihander.navup;
+package com.zweihander.navup.gis;
+
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+@EnableAutoConfiguration
+@ComponentScan("com.zweihander.navup.gis")
 public class GisApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/zweihander/navup/gis/domain/GISDataObject.java
+++ src/main/java/com/zweihander/navup/gis/domain/GISDataObject.java
@@ -1,35 +1,60 @@
 package com.zweihander.navup.gis.domain;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
-public class GISDataObject implements Serializable{
-    private double[] gpsCoords = new double[2];
-    private List<String> gpsTags = new ArrayList<>();
+/**
+ *
+ * @author Zweihander-GIS
+ */
 
-    public GISDataObject() {
-
-    }
-
-    public GISDataObject(GISDataObject gisDataObject) {
-        this.gpsCoords = gisDataObject.gpsCoords;
-        this.gpsTags = gisDataObject.gpsTags;
-    }
-
-    public double[] getGpsCoords() {
-        return gpsCoords;
-    }
-
-    public void setGpsCoords(double[] gpsCoords) {
-        this.gpsCoords = gpsCoords;
-    }
-
-    public List<String> getGpsTags() {
-        return gpsTags;
-    }
-
-    public void setGpsTags(List<String> gpsTags) {
-        this.gpsTags = gpsTags;
-    }
+public class GISDataObject 
+{
+   private double[] GPSCoord = new double[2];
+   private String objectName;
+   private List<String> GPSTags;
+   
+   public GISDataObject()
+   {
+       GPSTags = null;
+       
+       for(int i = 0;i<GPSCoord.length;i++)
+       {
+           GPSCoord[i] = 0.0;
+       }
+   }
+   public GISDataObject(double latitude,double longitude, String mObjectName,List<String> tags)
+   {
+       GPSCoord[0] = latitude;
+       GPSCoord[1] = longitude;
+       objectName = mObjectName ;
+       GPSTags = tags;
+   }
+   public double[] getGPSCoord() 
+   {
+        return this.GPSCoord;
+   }
+   public void setGPSCoord(double[] GPSCoord)
+   {
+        this.GPSCoord = GPSCoord;
+   }
+   public List<String> getGPSTags() 
+   {
+        return GPSTags;
+   }
+   public void setGPSTags(List<String> GPSTags)
+   {
+        this.GPSTags = GPSTags;
+   }
+   public void addTag( String tag) 
+   {
+        this.GPSTags.add(tag);
+   }
+   public String getObjectName() 
+   {
+        return objectName;
+   }
+   public void setObjectName(String objectName) 
+   {
+        this.objectName = objectName;
+   }
 }
